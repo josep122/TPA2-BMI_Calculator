0037b7d479eaebc85284041fb0ece1a804dce2f0
root <root@LAPTOP-4O9OBULV.localdomain>
2023-04-13 00:31:14 +0700

    Update TPA2

--- script.js
+++ script.js
@@ -60,12 +60,11 @@ function countBmi() {
   document.querySelector("#result").innerHTML = bmi.toFixed(2);
 }
 
-// When the user clicks on <span> (x), close the modal
+//Clicks (x)
 span.onclick = function () {
   modal.style.display = "none";
 };
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
